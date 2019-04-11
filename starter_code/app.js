ce4d1a03d35d782782e4a53fe4aad233065ef5a2
Gary <gary909@gmail.com>
2019-04-11 14:11:56 +0200

    First Push

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -13,9 +12,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render("index");
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      //  console.log(beers[0])
+      res.render("beers", {
+        beers
+      });
+
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
+app.get('/randombeer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render("randomBeer", {
+        beers
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
 
 app.listen(3000);
